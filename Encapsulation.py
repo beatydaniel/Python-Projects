3a94b1f008333203803200c1096201d45421cf9a
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-06-27 14:21:58 -0700

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -6,22 +6,15 @@ class Protected:
         
     def getPrivate(self): #function that prints out the private variable
         print(self.__privateVar)
-        
-    def getProtected(self): #function that prints out the protected variable
-        print(self._protectedVar)
-
-    def setProtected(self,protected): #sets the protected variable to be a new value
-        self._protectedVar=protected
-        
+                
     def setPrivate(self, private): #sets the private variable to be a new value
         self.__privateVar= private
 
         
 obj = Protected() #set obj to call the class Protected
 obj.getPrivate() #gets the original private value
-obj.getProtected() #gets the original protected value
-obj.setProtected(3) #changes the protected value
-obj.getProtected() #call to print the value of the changed protected variable
+obj._protectedVar = 34
+print(obj._protectedVar)
 obj.setPrivate(3) #changes the private value
 obj.getPrivate() #calls to print the value of the changed private variable
     
