49396fce368354a6b244e57210f8626de921a817
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-01 16:17:42 -0700

    Update

--- python-projects/Hello_World/src/mainapp/mainapp/views.py
+++
@@ -1,8 +0,0 @@
-
-from django.http import HttpResponse
-from django.shortcuts import render
-from profiles.models import Profile
-
-def home(request):
-    profiles = Profile.objects.all()
-    return render(request, 'home.html', {'profiles': profiles})
