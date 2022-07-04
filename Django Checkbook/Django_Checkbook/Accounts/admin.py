30702ecb8cad010be7f2bad67cd678a79d8b14ef
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-04 12:30:44 -0700

    Update

--- Django Checkbook/Django_Checkbook/Accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Account
-from .models import Information
-
-admin.site.register(Account)
-admin.site.register(Information)
